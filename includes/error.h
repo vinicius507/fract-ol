4657abefe4a303c125e4922d93a4d71fcce6ffc0
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-29 10:11:02 -0300

    refactor: error -> raise

--- includes/error.h
+++ includes/error.h
@@ -16,10 +16,13 @@
 /* Exit Codes for `fractol` */
 typedef enum e_error
 {
-	UNKNOWN_ARG,
-	NO_ARG,
+	UNKNOWN_ARG	= 1<<0,
+	NO_ARG = 1<<1,
+	NO_FRACTAL_OPTIONS = 1<<2,
+	INVALID_FRACTAL_OPTIONS = 1<<3,
+	SYS_ERROR = 1<<4,
 }	t_error;
 
-void	arg_error(const char *fractol, const char *argument, t_error code);
+void	raise(t_error code, const char *argument, const char *fractol);
 
 #endif
--- src/error.c
+++ src/error.c
@@ -12,12 +12,23 @@
 
 #include "fractol.h"
 
-void	arg_error(const char *fractol, const char *argument, t_error code)
+static void	arg_error_msg(t_error code, const char *arg, const char *fractol)
 {
 	if (code == UNKNOWN_ARG)
-		printf("%s: unknown argument '%s'\n", fractol, argument);
+		printf("%s: unknown argument '%s'\n", fractol, arg);
 	else if (code == NO_ARG)
 		printf("%s: no argument given\n", fractol);
+	else if (code == NO_FRACTAL_OPTIONS)
+		printf("%s: no options passed to fractal(1 required)\n", fractol);
+	else if (code == INVALID_FRACTAL_OPTIONS)
+		printf("%s: invalid option passed to fractal '%s'\n", fractol, arg);
+}
+
+void	raise(t_error code, const char *arg, const char *fractol)
+{
+	if (code
+		& (UNKNOWN_ARG | NO_ARG | NO_FRACTAL_OPTIONS | INVALID_FRACTAL_OPTIONS))
+		arg_error_msg(code, arg, fractol);
 	print_help(fractol);
 	exit(code);
 }
