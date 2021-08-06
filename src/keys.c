4d4303d4d068ace7f1a5c98522b83ee4122a7143
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-06 13:44:15 -0300

    feat: movement keys

--- src/keys.c
+++ src/keys.c
@@ -16,38 +16,28 @@
 int	keys(int key, t_fractol *fractol)
 {
 	if (key == ESC_KEY || key == Q_KEY)
-	{
-		mlx_destroy_window(fractol->mlx, fractol->window);
 		kill(fractol, 0);
-	}
 	else if (key == R_KEY)
-	{
 		set_default_viewport(fractol);
-		display_fractal(fractol);
-	}
-	else
-		printf("Key Value: %d\n", key);
+	else if (key == W_KEY || key == UP_KEY)
+		fractol->offset_y += 0.05L * fractol->scale;
+	else if (key == A_KEY || key == LEFT_KEY)
+		fractol->offset_x += 0.05L * fractol->scale;
+	else if (key == S_KEY || key == DOWN_KEY)
+		fractol->offset_y -= 0.05L * fractol->scale;
+	else if (key == D_KEY || key == RIGHT_KEY)
+		fractol->offset_x -= 0.05L * fractol->scale;
+	display_fractal(fractol);
 	return (0);
 }
 
 int	mouse_keys(int key, unsigned int px, unsigned int py, t_fractol *fractol)
 {
 	if (key == MWHEEL_UP)
-	{
 		zoom(1, fractol, px, py);
-		display_fractal(fractol);
-	}
 	else if (key == MWHEEL_DOWN)
-	{
 		zoom(-1, fractol, px, py);
-		display_fractal(fractol);
-	}
-	else
-		printf("%d: %Lf %d: %Lf\n",
-			px,
-			px * fractol->factor_x - fractol->offset_x,
-			py,
-			-(py * fractol->factor_y - fractol->offset_y));
+	display_fractal(fractol);
 	return (0);
 }
 
